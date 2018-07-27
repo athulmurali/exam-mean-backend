77b8a97b8597e28692341e5f6ed884c3dfd6c61a
Athul Muralidharan <athulmurali@gmail.com>
2018-07-27 18:38:46 +0530

    namesake change

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 
 module.exports={
     mongo :{
-        url:'mongodb://athulmurali:abcd1234@ds137687.mlab.com:37687/exam-manager'
+        url: 'mongodb://athulmurali:abcd1234@ds137687.mlab.com:37687/exam-manager'
     },
     jwt: {
         secret : "hakunaMatata"}
