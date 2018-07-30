7747876ff5e4662034e5a229bd2dd60d002201c2
Athul Muralidharan <athulmurali@gmail.com>
2018-07-30 23:28:19 +0530

    added courseId to section model

--- models/section.js
+++ models/section.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const UserSchema = require("./user");
 
 var Section =  mongoose.Schema({
+    courseId: Number,
     totalSeats : Number,
     title : {
         type : String
