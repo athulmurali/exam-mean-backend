c1ac0a4c7c3b325ca1f7a2e6ad4f3e122cd6dec4
Athul Muralidharan <athulmurali@gmail.com>
2018-08-08 21:41:19 +0530

    Deleted unwanted routes. Cleaned app.js

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var path            = require('path');
 var cookieParser    = require('cookie-parser');
 var logger          = require('morgan');
 
-var indexRouter     = require('./routes/index');
-var usersRouter     = require('./routes/users');
-var coursesRouter   = require('./routes/courses')
+
 
 var cors = require('cors')
 
@@ -43,9 +41,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-// app.use('/courses', coursesRouter);
+
 
 const apiRoutes= require('./routes/api')
 app.use('/api',apiRoutes);
